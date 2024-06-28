3b2d30f6cfe1b843c8aa1ea55792ec4d288852fb
Shawn <shawnthewerber@gmail.com>
2024-06-27 21:16:09 -0600

    gallery active text button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,21 @@ import ContactCTA from "@/components/ContactCTA";
 import TwoColumn from "@/components/TwoColumn";
 import BannerList from "@/components/BannerList";
 import { cataracts, dr, glaucoma, lasik, md } from "../../public/eyes/eyes.js";
+import {
+  anderson,
+  christian,
+  duncan,
+  godwin,
+  olson,
+} from "../../public/dop/dop";
+import { StaticImageData } from "next/image";
 import twoColumnImage from "../../public/gallery/lasik.webp";
 
 const Home = () => {
   const headerText = [
     "Mountain Eye Institute is where Southern Utah goes for Eye Care!",
   ];
+
   const bodyText = `Regardless of how old you are, it's important to have an ophthalmologist in Cedar City, UT that you can rely on. Ophthalmology specialists are integral for primary eye care and surgical procedures for your eye health. If you are looking for quality eye care, look to Mountain Eye Institute. Our complete ophthalmology practice uses only the most advanced practices and tools. Our board-certified ophthalmologists provide each of our clients with a wide range of optometrist services. Each person on our friendly and skilled team is prepared to bring exceptional eye services to those in Cedar City, UT.`;
 
   const galleryItems = [
@@ -97,6 +106,140 @@ const Home = () => {
     },
   ];
 
+  interface People {
+    image: StaticImageData;
+    alt: string;
+    fname: string;
+    lname: string;
+    title: string;
+    bio: React.JSX.Element;
+  }
+
+  const people: People[] = [
+    {
+      image: anderson,
+      alt: "Dr. Anderson",
+      fname: "Chad W.",
+      lname: "Anderson",
+      title: "M.D., FAAO",
+      bio: (
+        <p className='bio'>
+          Dr. Anderson is a leading eye surgeon with expertise in the latest
+          surgical and medical <a href='/treatments'>treatments</a> for eye
+          disease including treatment for: Cataracts, Glaucoma, Macular
+          Degeneration and Diabetes. After graduating Magna Cum Laude from
+          Southern Utah University, Dr. Anderson received his medical degree
+          from Loma Linda University school of Medicine in California where he
+          received the Macpherson Society Research Fellowship Award. He then
+          completed a Research Fellowship in Ophthalmic Pathology at the
+          University of Utah Medical Center. During his surgical residency in
+          Ophthalmology at the University of Kentucky, he received the
+          Outstanding Resident Teaching Award, The Outstanding Resident Research
+          Award, and served as Chief Resident. In 1995 he established Mountain
+          Eye Clinic in Cedar City and serves as Medical Director. Since joining
+          the medical staff of Valley View Medical Center, Dr. Anderson has
+          served as Chief of Surgery (97-98), Chief of Staff (98-99), and is
+          currently serving as Credentials Committee chairman and delegate to
+          the Utah Medical Association. Dr. Anderson is Board Certified as a
+          member of the American Academy of Ophthalmology. In his free time he
+          enjoys his family, scuba diving, skiing and cycling.`
+        </p>
+      ),
+    },
+    {
+      image: christian,
+      alt: "Dr. Christian",
+      fname: "Rory J.",
+      lname: "Christian",
+      title: "O.D.",
+      bio: (
+        <p className='bio'>
+          Dr. Duncan practices surgical and medical ophthalmology in our
+          practice. With expertise in the latest medical and surgical{" "}
+          <a href='/treatments'>treatments</a> for eye diseases including:
+          Cataracts, Glaucoma, Macular Degeneration, Diabetes and Pediatric
+          Strabismus surgery. After graduating Summa Cum Laude from Southern
+          Utah University, Dr. Duncan received his medical degree from
+          Pennsylvania State University College of Medicine where he received
+          the Dr. Irwin L. Baird Memorial Award and The Berman Memorial
+          Scholarship in Ophthalmology. He served his residency at University of
+          Texas health Center at San Antonio Department of Ophthalmology. Where
+          he was chief of Staff before joining Mountain Eye Institute.
+        </p>
+      ),
+    },
+    {
+      image: duncan,
+      alt: "Dr. Duncan",
+      fname: "Clinton",
+      lname: "Duncan",
+      title: "M.D.",
+      bio: (
+        <p className='bio'>
+          Dr. Duncan practices surgical and medical ophthalmology in our
+          practice. With expertise in the latest medical and surgical{" "}
+          <a href='/treatments'>treatments</a> for eye diseases including:
+          Cataracts, Glaucoma, Macular Degeneration, Diabetes and Pediatric
+          Strabismus surgery. After graduating Summa Cum Laude from Southern
+          Utah University, Dr. Duncan received his medical degree from
+          Pennsylvania State University College of Medicine where he received
+          the Dr. Irwin L. Baird Memorial Award and The Berman Memorial
+          Scholarship in Ophthalmology. He served his residency at University of
+          Texas health Center at San Antonio Department of Ophthalmology. Where
+          he was chief of Staff before joining Mountain Eye Institute.
+        </p>
+      ),
+    },
+    {
+      image: godwin,
+      alt: "Dr. Godwin",
+      fname: "Joseph",
+      lname: "Godwin",
+      title: "ABOC",
+      bio: (
+        <p className='bio'>
+          Joseph L. Godwin has been certified by the American Board of Opticians
+          since 1988. He began pursuing his career in the Optical industry after
+          enlisting in the US Army in 1983. In 1986 He began eye wear
+          manufacturing for Lens Crafters in Brea, Ca. He supervised an optical
+          retail store: “Eyes on Laguna” in Laguna Beach, Ca. till 1990 and
+          optical manufacturing for Drs Cohen and Cohen in Downey, Ca. until
+          1994. He relocated to beautiful Southern Utah for employment with
+          Ronald L. Snow M.D. in St George, Utah in 1994. He has been the
+          optical manager for Chad W Anderson M.D. since 2008. Joseph is a
+          Certified Optician with an expertise in providing you with the correct
+          prescription and the correct frames for all ages. He is a Certified
+          Optician with an expertise in providing you with the correct
+          prescription and the correct frames for all ages.
+        </p>
+      ),
+    },
+    {
+      image: olson,
+      alt: "Dr. Olson",
+      fname: "C. Kelly",
+      lname: "Olson",
+      title: "O.D., MBA",
+      bio: (
+        <p className='bio'>
+          `Dr. Olson retired from the U.S. Army after a 30-year distinguished
+          career, achieving the rank of Colonel. He and his wife, Valarie, have
+          returned to their roots by coming home to Southern Utah. Originally
+          from Fillmore, Dr. Olson graduated Summa Cum Laude from Southern Utah
+          University where he earned the R. Kenneth Benson Award, SUU&apos;s
+          highest student award. He earned his optometry degree at Southern
+          California College of Optometry and then completed an MBA and
+          Optometric residency at Northeastern State University. Dr. Olson is
+          excited to serve our practice using his advanced training and vast
+          experience with specialty contact lenses including scleral lenses. Dr.
+          Olson enjoys doing a variety of activities with his family in
+          beautiful Southern Utah. Some of these include camping, backpacking,
+          fishing, woodworking, pickleball and just being together.
+        </p>
+      ),
+    },
+  ];
+
   return (
     <>
       <TopNav />
@@ -122,7 +265,7 @@ const Home = () => {
         </a>
         <h2>Meet the Doctors</h2>
         <h3>The Best in Southern Utah</h3>
-        <DOP type={"round"} />
+        <DOP type={"round"} people={people} />
         <ContactCTA />
       </main>
       <Footer contact={true} tall={true} />
--- src/components/DOP.tsx
+++ src/components/DOP.tsx
@@ -1,13 +1,6 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
 import { useState, useRef, useEffect, LegacyRef } from "react";
-import {
-  anderson,
-  christian,
-  duncan,
-  godwin,
-  olson,
-} from "../../public/dop/dop";
 import React from "react";
 
 interface People {
@@ -16,78 +9,13 @@ interface People {
   fname: string;
   lname: string;
   title: string;
-  bio: string;
+  bio: React.JSX.Element;
 }
 
-const DOP = ({ type }: { type: string }) => {
+const DOP = ({ type, people }: { type: string; people: People[] }) => {
   const [open, setOpen] = useState("modal");
   const [modalPerson, setModalPerson] = useState(0);
   const modalRef = useRef<HTMLDivElement>();
-
-  const people: People[] = [
-    {
-      image: anderson,
-      alt: "Dr. Anderson",
-      fname: "Chad W.",
-      lname: "Anderson",
-      title: "M.D., FAAO",
-      bio: `Dr. Anderson is a leading eye surgeon with expertise in the latest surgical and medical ${(
-        <a href='/treatments'>treatments</a>
-      )} for eye disease including treatment for: Cataracts, Glaucoma, Macular Degeneration and Diabetes.
-
-After graduating Magna Cum Laude from Southern Utah University, Dr. Anderson received his medical degree from Loma Linda University school of Medicine in California where he received the Macpherson Society Research Fellowship Award. He then completed a Research Fellowship in Ophthalmic Pathology at the University of Utah Medical Center. During his surgical residency in Ophthalmology at the University of Kentucky, he received the Outstanding Resident Teaching Award, The Outstanding Resident Research Award, and served as Chief Resident. In 1995 he established Mountain Eye Clinic in Cedar City and serves as Medical Director.
-
-Since joining the medical staff of Valley View Medical Center, Dr. Anderson has served as Chief of Surgery (97-98), Chief of Staff (98-99), and is currently serving as Credentials Committee chairman and delegate to the Utah Medical Association.
-
-Dr. Anderson is Board Certified as a member of the American Academy of Ophthalmology. In his free time he enjoys his family, scuba diving, skiing and cycling.`,
-    },
-    {
-      image: christian,
-      alt: "Dr. Christian",
-      fname: "Rory J.",
-      lname: "Christian",
-      title: "O.D.",
-      bio: `Dr. Duncan practices surgical and medical ophthalmology in our practice. With expertise in the latest medical and surgical ${(
-        <a href='/treatments'>treatments</a>
-      )} for eye diseases including: Cataracts, Glaucoma, Macular Degeneration, Diabetes and Pediatric Strabismus surgery.
-
-After graduating Summa Cum Laude from Southern Utah University, Dr. Duncan received his medical degree from Pennsylvania State University College of Medicine where he received the Dr. Irwin L. Baird Memorial Award and The Berman Memorial Scholarship in Ophthalmology. He served his residency at University of Texas health Center at San Antonio Department of Ophthalmology. Where he was chief of Staff before joining Mountain Eye Institute.`,
-    },
-    {
-      image: duncan,
-      alt: "Dr. Duncan",
-      fname: "Clinton",
-      lname: "Duncan",
-      title: "M.D.",
-      bio: `Dr. Duncan practices surgical and medical ophthalmology in our practice. With expertise in the latest medical and surgical ${(
-        <a href='/treatments'>treatments</a>
-      )} for eye diseases including: Cataracts, Glaucoma, Macular Degeneration, Diabetes and Pediatric Strabismus surgery.
-
-After graduating Summa Cum Laude from Southern Utah University, Dr. Duncan received his medical degree from Pennsylvania State University College of Medicine where he received the Dr. Irwin L. Baird Memorial Award and The Berman Memorial Scholarship in Ophthalmology. He served his residency at University of Texas health Center at San Antonio Department of Ophthalmology. Where he was chief of Staff before joining Mountain Eye Institute.`,
-    },
-    {
-      image: godwin,
-      alt: "Dr. Godwin",
-      fname: "Joseph",
-      lname: "Godwin",
-      title: "ABOC",
-      bio: `Joseph L. Godwin has been certified by the American Board of Opticians since 1988. He began pursuing his career in the Optical industry after enlisting in the US Army in 1983. In 1986 He began eye wear manufacturing for Lens Crafters in Brea, Ca. He supervised an optical retail store: “Eyes on Laguna” in Laguna Beach, Ca. till 1990 and optical manufacturing for Drs Cohen and Cohen in Downey, Ca. until 1994. He relocated to beautiful Southern Utah for employment with Ronald L. Snow M.D. in St George, Utah in 1994. He has been the optical manager for Chad W Anderson M.D. since 2008.
-
-Joseph is a Certified Optician with an expertise in providing you with the correct prescription and the correct frames for all ages. He is a Certified Optician with an expertise in providing you with the correct prescription and the correct frames for all ages.`,
-    },
-    {
-      image: olson,
-      alt: "Dr. Olson",
-      fname: "C. Kelly",
-      lname: "Olson",
-      title: "O.D., MBA",
-      bio: `Dr. Olson retired from the U.S. Army after a 30-year distinguished career, achieving the rank of Colonel. He and his wife, Valarie, have returned to their roots by coming home to Southern Utah.
-
-Originally from Fillmore, Dr. Olson graduated Summa Cum Laude from Southern Utah University where he earned the R. Kenneth Benson Award, SUU’s highest student award. He earned his optometry degree at Southern California College of Optometry and then completed an MBA and Optometric residency at Northeastern State University. Dr. Olson is excited to serve our practice using his advanced training and vast experience with specialty contact lenses including scleral lenses.
-
-Dr. Olson enjoys doing a variety of activities with his family in beautiful Southern Utah. Some of these include camping, backpacking, fishing, woodworking, pickleball and just being together.`,
-    },
-  ];
 
   useEffect(() => {
     const handleKeyDown = (event: { key: string }) => {
@@ -199,7 +127,7 @@ Dr. Olson enjoys doing a variety of activities with his family in beautiful Sout
                 <p>{people[modalPerson].title}</p>
               </div>
             </div>
-            <p className='bio'>{people[modalPerson].bio}</p>
+            {people[modalPerson].bio}
           </div>
           <a id='close' onClick={() => setOpen("modal")}></a>
         </div>
